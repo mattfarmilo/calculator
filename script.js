40008ed6c844bdf43cb76f3f6fa3c222a97d92b3
mattfarmilo <longrnr56@gmail.com>
2023-05-23 19:51:30 +1000

    Create basic javascript calculator functions
    Created javascript functions for addition, subtraction, multiplication
    and division.  Declared global variables and created a selector
    function to run correct basic function based on the operator chosen.
    
    [Ticket: X]

---
+++ script.js
@@ -0,0 +1,39 @@
+//Basic calculation functions
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+//Global variables
+let answer = 0;
+let newNumber = 0;
+let operator;
+
+//Function to determine calculation mode
+function operate (num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+  }
+}
