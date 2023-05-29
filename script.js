81704e312a1521a410a33d643f5e494df67c35d1
mattfarmilo <longrnr56@gmail.com>
2023-05-29 20:55:39 +1000

    Event listeners for all buttons and formatting
    Fully working calculator.  Added event listeners for all buttons and
    styling.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//Global variables
+let answer = null;
+let newNumber = "0";
+let operator;
+
+//Display variables to screen
+function display () {
+  const entry = document.querySelector("#entry");
+  entry.textContent = newNumber;
+
+  const holding = document.querySelector("#holding");
+  holding.textContent = answer;
+
+  const sign = document.querySelector("#sign");
+  sign.textContent = operator;
+}
+display();
+
+
 //Basic calculation functions
 function add (a, b) {
   return a + b;
@@ -15,25 +34,85 @@ function divide (a, b) {
   return a / b;
 }
 
-//Global variables
-let answer = 0;
-let newNumber = 0;
-let operator;
-
 //Function to determine calculation mode
 function operate (num1, num2, operator) {
   switch (operator) {
-    case 'plus':
-      return add(num1, num2);
+    case '+':
+      return parseFloat(add(num1, num2).toFixed(5));
       break;
-    case 'minus':
-      return subtract(num1, num2);
+    case '-':
+      return parseFloat(subtract(num1, num2).toFixed(5));
       break;
-    case 'multiply':
-      return multiply(num1, num2);
+    case '*':
+      return parseFloat(multiply(num1, num2).toFixed(5));
       break;
-    case 'divide':
-      return divide(num1, num2);
+    case '/':
+    return parseFloat(divide(num1, num2).toFixed(5));
       break;
   }
 }
+
+//Button event listeners
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    if(newNumber === "0") {
+      newNumber = e.target.value;
+    } else {
+      newNumber = newNumber + e.target.value;
+    }
+    display();
+  });
+});
+
+const signs = document.querySelectorAll(".sign");
+signs.forEach((sign) => {
+  sign.addEventListener('click', (e) => {
+    if (answer === null) {
+      operator = e.target.value;
+      answer = parseFloat(newNumber);
+      newNumber = "0";
+    } else {
+      answer = operate(answer, parseFloat(newNumber), operator);
+      operator = e.target.value;
+      newNumber = "0";
+    }
+    display();
+  });
+});
+
+const equals = document.querySelector(".equals");
+equals.addEventListener('click', (e) => {
+  if(operator) {
+    newNumber = operate(answer, parseFloat(newNumber), operator);
+    operator = e.target.value;
+    answer = null;
+    display();
+  }
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', () => {
+  newNumber = "0";
+  answer = null;
+  operator = "";
+  display();
+});
+
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener('click', () => {
+  if (newNumber.length > 1) {
+    newNumber = newNumber.slice(0,-1);
+  } else {
+    newNumber = "0";
+  }
+  display();
+});
+
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener('click', () => {
+  if(!newNumber.includes(".")) {
+    newNumber = newNumber + ".";
+  }
+  display();
+});
